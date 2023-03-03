da8e27d42c8627c65a288f658fc09d1cb8402836
Ali Abed <aliabed@Alis-Air.attlocal.net>
2023-03-03 15:20:02 -0600

    done

--- my_personal_website/settings.py
+++ my_personal_website/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-uk%6vj8y4ln$#7)wlu$0co&c(q)_ynogs*q2%e4youj&y(w&8t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIR = [
     os.path.join(BASE_DIR, 'static'), 

15162d2e3578d26a6f10010bc0fae2cf1ddf8417
Ali Abed <aliabed@Alis-Air.attlocal.net>
2023-05-29 11:46:27 -0600

    blogs edit

--- main/models.py
+++ main/models.py
@@ -2,10 +2,8 @@ from django.db import models
 
 
 class Post(models.Model):
-    upload = models.ImageField(upload_to ='uploads/')
     title = models.CharField(max_length=200, unique=True)
     desci = models.CharField(max_length=300, default= '')
-    tag = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
--- my_personal_website/settings.py
+++ my_personal_website/settings.py
@@ -124,12 +124,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'staticfiles/'
-STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+#STATICFILES_DIRS = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-STATICFILES_DIR = [
-    os.path.join(BASE_DIR, 'static'), 
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
